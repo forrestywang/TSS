fa092b33f18d459a7148f03dc2db19d3c1573133
Forrest Wang <95055762+forrestywang@users.noreply.github.com>
2023-06-08 17:08:06 -0400

    Uploaded the Testing folder from PC

---
+++ Testing/Template.java
@@ -0,0 +1,35 @@
+package Testing;    // Need to change package.
+
+// @author Forrest Wang
+// Month Day, 2022
+//
+// [Program Name]
+// [Program Description]
+
+// Import(s):
+import java.util.*;
+import java.io.*;
+import java.text.*;
+
+public class Template {    // Need to change Program Name.
+    public static void main(String[] args) {
+        Scanner myScanner = new Scanner(System.in);
+        Random rng = new Random();
+
+        // Constants:
+
+
+        // Variables:
+
+        myScanner.close();
+
+        // Formatting:
+
+
+        // Calculations:
+
+
+        // Output:
+        System.out.println("");
+    }
+}
---
+++ Testing/Testing.java
@@ -0,0 +1,35 @@
+package Testing;
+
+// @author Forrest Wang
+// Month Day, 2021
+//
+// Testing_Copy
+// For testing Java applications.
+
+// Imports:
+import java.util.*;
+import java.io.*;
+import java.text.*;
+
+public class Testing {
+    public static void main(String[] args) {
+        Scanner myScanner = new Scanner(System.in);
+        Random rng = new Random();
+
+        // Constants:
+
+
+        // Variables:
+
+        myScanner.close();
+
+        // Formatting:
+
+
+        // Calculations:
+
+
+        // Output:
+        System.out.println();
+    }
+}
